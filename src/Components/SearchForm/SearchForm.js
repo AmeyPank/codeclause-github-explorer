12255d3472cf7d98fbf0f29616d2ed6fa51c7735
AmeyPank <pankantiameya@gmail.com>
2023-06-07 00:02:42 +0530

    Added Responsiveness

--- src/Components/SearchForm/SearchForm.js
+++ src/Components/SearchForm/SearchForm.js
@@ -2,30 +2,41 @@ import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { fetchUserData } from '../../Redux/actions';
 import { AiOutlineSearch } from 'react-icons/ai';
-import "./searchform.css"
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import background from "./explore-github.png"
+import "./searchform.css"
+
 const SearchForm = () => {
     const [username, setUsername] = useState('');
     const dispatch = useDispatch();
 
-    if (username) {
-        toast("error")
-    }
     const handleSubmit = (e) => {
         e.preventDefault();
+
+        if (!username) {
+            toast.error('Username cannot be empty.');
+            return;
+        }else{
+            toast.success('Username is available.');
+
+        }
+
         dispatch(fetchUserData(username));
     };
 
     return (
         <div className="searchBar-container">
+            <div className='image-container'>
+                <img src={background} alt='imag' />
+            </div>
             <form className="search-form" onSubmit={handleSubmit}>
                 <h1>GitHub Explorer</h1>
                 <div className="search-input-container">
                     <input
                         className="search-input"
                         type="text"
-                        placeholder="Enter GitHub username"
+                        placeholder="Enter Github username..."
                         value={username}
                         onChange={(e) => setUsername(e.target.value)}
                     />
